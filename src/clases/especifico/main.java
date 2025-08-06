e03d537cea577ab252da4f8c4343089e7ce69246
nicso09 <ln.aguirre2005@gmail.com>
2025-08-06 00:22:14 -0300

    versionFinalConTest0.1

--- src/clases/especifico/main.java
+++ src/clases/especifico/main.java
@@ -3,14 +3,13 @@ package clases.especifico;
 public class main {
     public static void main(String[] args) {
         DiccionarioAVL arbol = new DiccionarioAVL();
-        String val1 = "a";
-        String val2 = "b";
-        String val3 = "c";
-        String obj1 = "yogur";
-        arbol.insertar(val1, obj1);
-        arbol.insertar(val2, obj1);
-        arbol.insertar(val3, obj1);
-
-
+        String a = "a";
+        int[] claves = {30, 20, 50, 15, 24, 35, 100, 12, 18, 22, 27, 33, 40, 90, 105, 13, 17, 19, 23, 37, 16};
+        for (int i = 0; i < claves.length; i++) {
+            arbol.insertar(claves[i], a);
+        }
+        System.out.println(arbol.toString());
+        arbol.eliminar(30);
+        System.out.println(arbol.toString()); // Elimina de manera correcto el nodo con clave 30 y rebalancea
     }
 }
